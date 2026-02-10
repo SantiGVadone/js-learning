c40a6567ad600a555bc49704069502676e51fada
SantiGVadone <santiagovadone36@gmail.com>
2026-02-10 15:55:24 -0300

    practiced session 1

---
+++ 01-basics/beginner-exercises.js
@@ -0,0 +1,29 @@
+//imprimir en consola el tipo de una variable
+/*
+let contraseña = '123'
+console.log(typeof contraseña+" "+contraseña)
+contraseña = 123
+console.log(typeof contraseña+" "+contraseña )
+contraseña = 1.2
+console.log(typeof contraseña+" "+contraseña)
+contraseña = null
+console.log(typeof contraseña+" "+contraseña)
+contraseña = undefined
+console.log(typeof contraseña+" "+contraseña)
+contraseña = {}
+console.log(typeof contraseña+" "+contraseña)
+contraseña = []
+console.log(typeof contraseña+" "+contraseña)
+contraseña= true
+console.log(typeof contraseña+" "+contraseña)
+*/
+//jugar con los operadores
+console.log(2>3)
+
+console.log(2<3)
+
+console.log(2<3 || 2>3)
+
+console.log(2<3 && 2>3)
+
+console.log(!(2<3))
